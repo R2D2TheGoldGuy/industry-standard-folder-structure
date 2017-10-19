57df62787d7a1afdde774ef1ca46e48041e58897
Kendrick Chung <kenchung9001@gmail.com>
2017-10-20 03:43:03 +1100

    Industry standard folder structure template.

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php');
+perch_layout('default-top'); ?>
+
+<!-- HEADER -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/partials/header.php' ?>
+<!-- INTRO -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/partials/intro.php' ?>
+<!-- EXPERIENCE -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/partials/experience.php' ?>
+<!-- TESTIMONIALS -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/partials/testimonials.php' ?>
+<!-- WHAT YOU GET -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/partials/alt-package.php' ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/partials/cta.php' ?>
+
+<?php perch_layout('default-bottom'); ?>
